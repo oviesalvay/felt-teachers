e899931320cb2154816686b91837416068ac1666
oviesalvay <oviesalvay@gmail.com>
2023-09-12 15:09:01 -0800

    fix

--- src/views/registeration/log.tsx
+++ src/views/registeration/log.tsx
@@ -1,8 +1,30 @@
-
+import { Link } from 'react-router-dom';
+import group from '../../assets/group.png';
+import { Buttoned } from "../../components/button/button";
 
 const Log = () => {
     return (
         <>
+            <section className='join-intro'>
+                <p>Back to Home</p>
+                <div className='join'>
+                    <div>
+                        <img src={group} alt="" />
+                    </div>
+                    <div className='community'>
+                        <h4>Welcome Back,
+                            Log In To Your Account</h4>
+                        <p>Which Category Do You Belong</p>
+                        <Link to="/signUp">
+                            <Buttoned type="none" content="Teacher" /></Link>
+                        <Link to="/signS">
+                            <Buttoned type="none" content="School" /></Link>
+                        <Link to="/signing">
+                            <Buttoned type="none" content="Parent" /></Link>
+
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
